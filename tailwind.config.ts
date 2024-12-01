e82a4ba9b9f4e28bb226696c778881474b04ebec
Sakib Hassan <sakibhassan.webdev@gmail.com>
2024-12-01 12:04:29 +0600

    initial setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,30 +1,18 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      screens: {
-        xsm: "428px",
-      },
-      backgroundImage: {
-        "hero-bg": "url('/assets/images/hero-img.webp')",
-      },
-      keyframes: {
-        "swatch-pulse": {
-          "0%": { transform: "scale(1)" },
-          "100%": { transform: "scale(0.9)" },
-        },
-      },
-      animation: {
-        "swatch-pulse": "swatch-pulse 1.2s ease-in infinite alternate",
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
       },
     },
   },
   plugins: [],
-};
-export default config;
+} satisfies Config;
