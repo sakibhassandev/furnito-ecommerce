ca432734382cac66536f2d68ed81541e6815a367
Sakib Hassan <sakibhassan.webdev@gmail.com>
2024-12-10 20:30:35 +0600

    feat: add loading state to reset password and verify account pages using Suspense

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,12 +1,18 @@
 import type { Metadata } from "next";
 import { ResetPassword } from "@/ui/auth/ResetPassword";
+import { Suspense } from "react";
+import { Loader2 } from "lucide-react";
 
 export const metadata: Metadata = {
   title: "Reset Password",
 };
 
-const RegisterPage = () => {
-  return <ResetPassword />;
+const ResetPage = () => {
+  return (
+    <Suspense fallback={<Loader2 className="animate-spin w-7 h-7" />}>
+      <ResetPassword />;
+    </Suspense>
+  );
 };
 
-export default RegisterPage;
+export default ResetPage;
--- src/app/(auth)/verify/page.tsx
+++ src/app/(auth)/verify/page.tsx
@@ -1,12 +1,18 @@
 import type { Metadata } from "next";
 import { Verify } from "@/ui/auth/Verify";
+import { Suspense } from "react";
+import { Loader2 } from "lucide-react";
 
 export const metadata: Metadata = {
   title: "Verify Account",
 };
 
 const VerifyPage = () => {
-  return <Verify />;
+  return (
+    <Suspense fallback={<Loader2 className="animate-spin w-7 h-7" />}>
+      <Verify />
+    </Suspense>
+  );
 };
 
 export default VerifyPage;
