57f8fa213e9f577cd6b558c5ca59c8c2d7bcbf0f
Sakib Hassan <sakibhassan.webdev@gmail.com>
2025-01-15 23:24:47 +0600

    fix: remove unused Image import in MyProfile component

--- src/ui/common/MyProfile.tsx
+++ src/ui/common/MyProfile.tsx
@@ -11,7 +11,6 @@ import {
   User,
   Briefcase,
 } from "lucide-react";
-import Image from "next/image";
 import axios from "axios";
 import Link from "next/link";
 import { ProfileData } from "@/lib/definitions";
