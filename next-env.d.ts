54632c491be152d532b81415286fcc7aba963967
Sakib Hassan <sakibhassan.webdev@gmail.com>
2025-01-15 23:29:32 +0600

    feat: add TypeScript declaration for html2pdf.js module

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,3 +3,9 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
+
+declare module "html2pdf.js" {
+  const html2pdf: (element: HTMLElement, options?: object) => Promise<void>;
+
+  export default html2pdf;
+}
